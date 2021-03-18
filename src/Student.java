c0e248d4665cf36538bde485dfd5aae490a3cf9d
PaulVolk-Mac <75779886+PaulVolk-Mac@users.noreply.github.com>
2021-03-18 10:27:44 +0100

    should work now

--- src/Student.java
+++ src/Student.java
@@ -5,7 +5,7 @@ public class Student {
     private String lastName;
     private String firstName;
 
-    public Student(String iD,String firstName, String lastName, Set<Book>books) {
+    public Student(String iD,String firstName, String lastName) {
         this.iD = iD;
         this.firstName = firstName;
         this.lastName = lastName;
